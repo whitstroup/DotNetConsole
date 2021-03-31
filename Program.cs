e159beaf5ddda0479bdef40d7164036e2b085876
Whit Stroup <email@example.com>
2021-03-31 13:37:39 -0500

    created dotnet console

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace dotNetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+
+            var name = Console.ReadLine();
+
+
+            Console.WriteLine("Hello "+ name + "!");
+        }
+    }
+}
